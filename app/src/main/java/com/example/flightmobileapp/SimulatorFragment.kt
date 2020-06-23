82af16632c5a55a26817e456a1d9818682732b5e
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2020-06-23 15:26:00 +0300

    Finished handling simulator pictures
    still need to test

---
+++ app/src/main/java/com/example/flightmobileapp/SimulatorFragment.kt
@@ -0,0 +1,114 @@
+package com.example.flightmobileapp
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import com.example.flightmobileapp.databinding.FragmentSimulatorBinding
+import com.google.gson.GsonBuilder
+import kotlinx.coroutines.*
+import okhttp3.Dispatcher
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.InputStream
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SimulatorFragment : Fragment() {
+    private lateinit var binding: FragmentSimulatorBinding
+    private lateinit var simulatorJob: Job
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate<FragmentSimulatorBinding>(inflater,
+        R.layout.fragment_simulator, container, false)
+
+        return binding.root
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        val uri:String = "http://localhost:5410"
+
+        simulatorJob = Job()
+
+        val scope = CoroutineScope(Dispatchers.Main + simulatorJob)
+
+        scope.launch {
+            imageAsking(uri, 100)
+        }
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        simulatorJob.cancel()
+    }
+
+
+    private suspend fun imageAsking(uri: String, delayTime: Long) {
+        while (true) {
+            val url = uri + "screenshot/"
+
+            withContext(Dispatchers.Main) {
+                getImageFromServer(url)
+            }
+
+            delay(delayTime)
+        }
+    }
+
+
+    /**
+     * Get an image from the server.
+     * @param url the url to ask image from.
+     * @return the image from the server if succeeded.
+     */
+    private fun getImageFromServer(url: String) {
+        val gson = GsonBuilder()
+                .setLenient()
+                .create()
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+
+        val api = retrofit.create(Api::class.java)
+
+        api.getImg().enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(
+                    call: Call<ResponseBody>,
+                    response: Response<ResponseBody>
+            ) {
+                if (response.isSuccessful()) {
+                    // Save the response of the server as a byte stream.
+                    val res = response.body()?.byteStream()
+                    val picture = BitmapFactory.decodeStream(res!!)
+
+                    binding.simulatorView.setImageBitmap(picture)
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+            }
+        })
+    }
+}
